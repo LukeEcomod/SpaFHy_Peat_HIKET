56762750e43d94b44b43cb80b39a3e9d21ef0f0c
khaahti <kersti.haahti@luke.fi>
2020-04-09 15:25:17 +0300

    allas testing

---
+++ climate_data_from_allas.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 15:02:38 2020
+
+@author: 03110850
+"""
+
+import os
+import boto3
+
+fdir='climate_data'
+os.makedirs(fdir, exist_ok=True)
+bucket='project_2000611-weather-climate'
+rcp=['rcp26','rcp45']
+
+s3 = boto3.client('s3', Bucket=bucket, endpoint_url='https://a3s.fi')
+
+for object in s3.list_objects()['Contents']:
+    if len(object['Key'].split('.')) > 2:
+        if object['Key'].split('.')[-2] in rcp:
+            print(object['Key'])
+            s3.download_file(bucket, object['Key'], os.path.join(fdir,object['Key']))
