e566e56862331376c54aa4670c11dc557c7a66ab
khaahti <kersti.haahti@luke.fi>
2020-04-16 10:36:51 +0300

    outputfile merge

--- merge_outputfiles.py
+++ merge_outputfiles.py
@@ -8,10 +8,6 @@ import xarray as xr
 import os
 import pandas as pd
 
-outputfiles = ['results/sompa_data_2014-2019.nc','results/sompa_data_2014-2020.nc']
-
-merged_outputfile = 'results/test.nc'
-
 def to_montly_merge(outputfiles, merged_outputfile):
 
     results = []
@@ -30,4 +26,35 @@ def to_montly_merge(outputfiles, merged_outputfile):
 
     results.to_netcdf(merged_outputfile)
 
-to_montly_merge(outputfiles, merged_outputfile)
+if __name__ == '__main__':
+
+    outputfiles=[
+        'results/CanESM2_rcp26_1981-2010.nc',
+        'results/CanESM2_rcp26_2070-2099.nc',
+        'results/CanESM2_rcp45_1981-2010.nc',
+        'results/CanESM2_rcp45_2070-2099.nc',
+        'results/CNRM_rcp26_1981-2010.nc',
+        'results/CNRM_rcp26_2070-2099.nc',
+        'results/CNRM_rcp45_1981-2010.nc',
+        'results/CNRM_rcp45_2070-2099.nc',
+        'results/GFDL_rcp26_1981-2010.nc',
+        'results/GFDL_rcp26_2070-2099.nc',
+        'results/GFDL_rcp45_1981-2010.nc',
+        'results/GFDL_rcp45_2070-2099.nc',
+        'results/HadGEM2_rcp26_1981-2010.nc',
+        'results/HadGEM2_rcp26_2070-2099.nc',
+        'results/HadGEM2_rcp45_1981-2010.nc',
+        'results/HadGEM2_rcp45_2070-2099.nc',
+        'results/MIROC_rcp26_1981-2010.nc',
+        'results/MIROC_rcp26_2070-2099.nc',
+        'results/MIROC_rcp45_1981-2010.nc',
+        'results/MIROC_rcp45_2070-2099.nc',
+        ]
+
+    outputs = ['1981-2010', 'rcp26_2070-2099', 'rcp45_2070-2099']
+
+    for out in outputs:
+        outfiles = [i for i in outputfiles if out in i]
+        to_montly_merge(outfiles, 'results/monthly_' + out + '.nc')
+
+
