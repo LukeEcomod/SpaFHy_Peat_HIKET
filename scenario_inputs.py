72c191811a940b8c579a6fa3f6893e15d8f09e8d
khaahti <kersti.haahti@luke.fi>
2020-04-30 09:31:59 +0300

    saving scenario input without forcing

--- scenario_inputs.py
+++ scenario_inputs.py
@@ -14,12 +14,11 @@ import numpy as np
 from iotools import write_AsciiGrid
 
 def create_inputs(fp_forcing,
+                  write_forcing=False,
                   fp_coords=r'scen_coords\coordinates_all.txt',
-                  cf=[0.75, 0.65, 0.55, 0.45],
-                  LAI_conif=[5.5, 3.5, 2., 1.5],
-                  LAI_decid=[0., 0., 0., 0.],
-                  hc=20.0, soil_id=2,
-                  ditch_depth=0.5, ditch_spacing=40.0):
+                  ba=[ 6., 12., 18., 24., 30.],
+                  decid_frac=0.0, hc=21.0, soil_id=2,
+                  ditch_depth=0.4, ditch_spacing=50.0):
 
     # output directory
     fdir_output = '_'.join((os.path.basename(fp_forcing)).split('.')[:-1])
@@ -32,14 +31,17 @@ def create_inputs(fp_forcing,
 
     data = data['dat']
 
-    data['PAR'] = data['PAR'] * 1e6 / (24*3600) / 4.56  # mol/m2/day to W/m2
+    if write_forcing:
+        data['PAR'] = data['PAR'] * 1e6 / (24*3600) / 4.56  # mol/m2/day to W/m2
 
-    # rday is the day since 1980-01-01 (365 d calendar)
-    year = 1980. + (data['rday'] - 1 - ((data['rday'] - 1) % 365))/365
+        # rday is the day since 1980-01-01 (365 d calendar)
+        year = 1980. + (data['rday'] - 1 - ((data['rday'] - 1) % 365))/365
 
-    data['doy'] = (data['rday'] - 1) % 365 + 1
+        data['doy'] = (data['rday'] - 1) % 365 + 1
 
-    data['date'] = pd.to_datetime(year * 1000 + data['doy'], format='%Y%j')
+        data['date'] = pd.to_datetime(year * 1000 + data['doy'], format='%Y%j')
+    else:
+        data = data[data['rday'] == 1]
 
     coords_data = sorted(set(data['id']))
 
@@ -53,12 +55,13 @@ def create_inputs(fp_forcing,
 
     # plt.plot(inputs['E'], inputs['N'],'o')
 
-    # write forcing file for each grid point
-    fn = os.path.join(fdir_output,'forcing','weather_id_[forcing_id].csv')
-    os.makedirs(os.path.join(fdir_output, 'forcing'), exist_ok=True)
-    for idx in inputs['ID']:
-        data[data['id']==idx][['date','doy','TAir','Precip','PAR', 'VPD']].to_csv(
-            fn.replace('[forcing_id]',str(int(idx))), index=False)
+    if write_forcing:
+        # write forcing file for each grid point
+        fn = os.path.join(fdir_output,'forcing','weather_id_[forcing_id].csv')
+        os.makedirs(os.path.join(fdir_output, 'forcing'), exist_ok=True)
+        for idx in inputs['ID']:
+            data[data['id']==idx][['date','doy','TAir','Precip','PAR', 'VPD']].to_csv(
+                fn.replace('[forcing_id]',str(int(idx))), index=False)
 
     input_para = {}
 
@@ -75,7 +78,7 @@ def create_inputs(fp_forcing,
 
     imin = 0
     imax = len(inputs['E'])
-    jmax = len(cf)
+    jmax = len(ba)
 
     for key in keys:
         input_para[key] = np.ones([imax,jmax])
@@ -91,9 +94,9 @@ def create_inputs(fp_forcing,
     input_para['hc'][:,:] = hc
 
     for j in range(jmax):
-        input_para['cf'][:,j] = cf[j]
-        input_para['LAI_conif'][:,j] = LAI_conif[j]
-        input_para['LAI_decid'][:,j] = LAI_decid[j]
+        input_para['cf'][:,j] = 0.1939 * ba[j] / (0.1939 * ba[j] + 1.69)
+        input_para['LAI_conif'][:,j] = (1 - decid_frac) * (0.2197 * ba[j] + 0.0648)
+        input_para['LAI_decid'][:,j] = decid_frac * (0.2197 * ba[j] + 0.0648)
 
     fpath = os.path.join(fdir_output,'parameters')
     os.makedirs(fpath, exist_ok=True)
