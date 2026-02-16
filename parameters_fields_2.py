222c2da23ce40c24cb0ddb214e76e0e0738878d4
Jari-Pekka Nousu <jari-pekka.nousu@oulu.fi>
2026-02-16 10:04:05 +0100

    new forcing etc

--- parameters_fields_2.py
+++ parameters_fields_2.py
@@ -10,9 +10,9 @@ import time
 def parameters(folder=''):
 
     pgen = {'description': 'testcase',  # description written in result file
-            'start_date': '2015-01-01',
-            'end_date': '2020-12-31',
-            'spinup_end': '2015-12-31',
+            'start_date': '2019-01-01',
+            'end_date': '2025-12-31',
+            'spinup_end': '2020-12-31',
             'dt': 86400.0,
             'spatial_cpy': True,  # if False uses parameters from cpy['state']
             # else needs cf.dat, hc.dat, LAI_decid.dat, LAI_spruce.dat, LAI_pine.dat, (cmask.dat)
